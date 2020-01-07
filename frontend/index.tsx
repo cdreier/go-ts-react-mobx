6ccf294dc4dc42a0fec1222817f6dcb82cfa32ae
Christian Dreier <christian.dreier@inovex.de>
2020-01-07 12:33:36 +0100

    moved to new hotloader api and added basic test setup

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,27 +1,16 @@
+import { hot } from 'react-hot-loader/root'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 import App from './src/App'
 
 // import * as mobx from 'mobx'
 // mobx.useStrict(true)
 
-declare const module: any
+const RootComponent = hot(() => {
+  return <App />
+})
 
-renderWithHotReload(App)
-
-if (module.hot) {
-  module.hot.accept('./src/App', () => {
-    const NextRoot = require('./src/App.tsx').default
-    renderWithHotReload(NextRoot)
-  })
-}
-
-function renderWithHotReload(RootElement: any) {
-  ReactDOM.render(
-    <AppContainer>
-      <RootElement />
-    </AppContainer>,
-    document.getElementById('mount'),
-  )
-}
+ReactDOM.render(
+  <RootComponent />,
+  document.getElementById('mount'),
+)
--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -5,7 +5,7 @@ import { observer } from 'mobx-react-lite'
 const Editor = () => {
   const demo = useContext(DemoStore)
   return (
-    <input value={demo.demostring} onChange={e => demo.demostring = e.target.value} />
+    <input value={demo.demostring} onChange={e => demo.demostring = e.target.value} data-testid="editorinput" />
   )
 }
 
